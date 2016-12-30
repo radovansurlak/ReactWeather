c4c6bfa0a53f481e1d50e12b0c9a891fc776ef2d
radovansurlak <radovansurlak@gmail.com>
2016-12-30 20:22:01 +0100

    edited Examples.jsx

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,5 +1,12 @@
 let React = require('react');
 
-let Examples = (props) => <h3>Examples Component!</h3>;
+let Examples = (props) => {
+  return (
+    <div>
+      <h3>Examples component</h3>
+      <p>Welcome to examples page :)</p>
+    </div>
+  )
+};
 
 module.exports = Examples;
